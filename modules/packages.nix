efaed76df34233b8971387f5b40c293dd79288f0
Corentinrhr <corahier@hotmail.fr>
2026-05-05 23:02:10 +0200

    add pkg

--- modules/packages.nix
+++ modules/packages.nix
@@ -8,6 +8,7 @@
     unzip zip p7zip
     tree ripgrep fd bat
     gnome-tweaks
+    flameshot
 
     # ── GPU / graphics diagnostics ──────────────────────────────────────────
     vulkan-tools mesa-demos clinfo
